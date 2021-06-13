c26dffdd8dd9c6cc5e5560933d9ae86d1f42a635
Marco Filho <mrcantoniofilho@gmail.com>
2021-06-13 15:26:33 -0300

    Entry file added, tests deprecated

--- Entry.java
+++ Entry.java
@@ -1,25 +1,9 @@
-import Application.Controller.TicketsController;
-import Application.View.View;
-import Auth.Session;
-
-import tests.*;
+import Application.View.Home;
 
 public class Entry {
     public static void main(String[] args) {
         do {
-            if (!session.isUserAuthenticated())
-                View.index();
-            else
-                //TicketsController.index();
+            Home.index();
         } while(true);
-    }
-
-    static void tests() {
-        System.out.println("=== tests begin ===");
-
-        //Tests.testGSONstuff();
-        //Tests.testSaveChangesAndReadJsonFile();
-
-        System.out.println("=== end ===");
     }
 }
